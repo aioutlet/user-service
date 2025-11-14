49bcd928967a13dcb9fc344ae864b0e0372f9824
prasadhonrao <honrao.prasad@gmail.com>
2025-11-14 08:57:27 +0000

    feat: Implement DaprClientService for metadata operations and remove deprecated DaprClientHelper

---
+++ src/clients/dapr.client.service.js
@@ -0,0 +1,43 @@
+/**
+ * Dapr Client Helper
+ * Provides utilities for Dapr metadata operations
+ */
+import { DaprClient, CommunicationProtocolEnum } from '@dapr/dapr';
+import config from '../core/config.js';
+import logger from '../core/logger.js';
+
+class DaprClientService {
+  constructor() {
+    this.client = null;
+  }
+
+  getClient() {
+    if (!this.client) {
+      this.client = new DaprClient({
+        daprHost: config.dapr.host,
+        daprPort: config.dapr.httpPort,
+        communicationProtocol: CommunicationProtocolEnum.HTTP,
+      });
+    }
+    return this.client;
+  }
+
+  /**
+   * Get Dapr metadata
+   * @returns {Promise<object>} Dapr metadata
+   */
+  async getMetadata() {
+    try {
+      const client = this.getClient();
+      const metadata = await client.metadata.get();
+      return metadata;
+    } catch (error) {
+      logger.error('Failed to get Dapr metadata', { error: error.message });
+      throw error;
+    }
+  }
+}
+
+// Export singleton instance
+export const daprClient = new DaprClientService();
+export default daprClient;
--- src/clients/index.js
+++ src/clients/index.js
@@ -3,5 +3,5 @@
  * Exports clients for external service communication
  */
 
-export { default as daprClient } from './dapr.service.client.js';
+export { default as daprClient } from './dapr.client.service.js';
 export { secretManager, getDatabaseConfig, getJwtConfig } from './dapr.secret.manager.js';
